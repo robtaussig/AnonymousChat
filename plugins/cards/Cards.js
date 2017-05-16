6532bc363e787891913e8e5d0d40adf99005ccc9
Robert Taussig <robert.taussig@gmail.com>
2017-05-16 01:18:37 -0400

    Refactor Cards

--- plugins/cards/Cards.js
+++ plugins/cards/Cards.js
@@ -4,137 +4,24 @@ const constants = require('./constants.js');
 
 module.exports = class Cards {
   constructor(reply) {
-    this.games = {
-      
-    };
-
+    this.games = {};
     this.reply = reply;
-
-    this.playerCoins = {
-
-    };
-
+    this.playerCoins = {};
     this.availableCommands = [
       '\'/cards --start [game]\' - Start an instance of [game]. (example: \'/cards --start bj\'',
       '\'/cards --join [game]\' - Join a current instance of [game]. (example: \'/cards --join bj\'',
       '\'/cards --reset [game]\' - Reset a current instance of [game]. (example: \'/cards --reset bj\'',
       'Available games: [bj] (Blackjack)'
     ];
-
-  }
-
-  emitWarning(user, message) {
-    this.reply({
-      message: message,
-      user: user,
-      styling: {
-        color: 'red'
-      }
-    });
-  }
-
-  receiveCommand(user, command) {
-    // if (user) {
-    //   return this.emitWarning(user, 'Cards is currently in development. Features coming soon.');
-    // }
-
-    const parsedCommands = this.extractCommands(command);
-
-    if (parsedCommands.isValid) {
-      if (parsedCommands.start) {
-        this.startGame(user, parsedCommands);
-      }
-      else if (parsedCommands.help) {
-        this.getHelp(user, parsedCommands);
-      }
-      else if (parsedCommands.join) {
-        this.joinGame(user, parsedCommands);
-      }
-      else if (parsedCommands.bj && this.games.bj) {
-        this.games.bj.receiveCommand(user, parsedCommands);
-      }
-      else {
-        return this.emitWarning(user, 'You must issue one of the following commands: \'/cards --start [game] -[#players]\',\'/cards --join [game]\', or \'--help [game]/all\'');
-      }
-    }
-    else {
-      return this.emitWarning(user, parsedCommands.reason);
-    }
-  }
-
-  getHelp(user, parsedCommands) {
-    if (parsedCommands.help === 'all') {
-      this.availableCommands.forEach(command => {
-        this.reply({
-          user: user,
-          message: `${command}`,
-          styling: {
-            color: 'white'
-          }
-        });
-      });
-    }
-    else if (parsedCommands.help === 'bj') {
-      [
-        `\'/cards -bj -deal\' - Start a game with existing players`,
-        `\'/cards -bj -hit\' - Hit to draw another card`,
-        `\'/cards -bj -stand\' - Stand to keep your current card total`,
-        `\'/cards -bj -double\' - Double your bet and draw exactly once more (feature still in development)`,
-      ].forEach(command => {
-        this.reply({
-          user: user,
-          message: `${command}`,
-          styling: {
-            color: 'white'
-          }
-        });
-      });
-    }
-    else {
-      this.emitWarning(user, 'Valid options for help: \'/cards --help all\' and \'/cards --help bj\' (Blackjack)');
-    }
-  }
-
-  joinGame(user, parsedCommands) {
-    switch (parsedCommands.join.toLowerCase()) {
-      case 'bj':
-        if (this.games.bj) {
-          this.games.bj.addPlayer(user);
-        }
-        else {         
-          this.emitWarning(user, 'There is no open game of blackjack. Type /cards --start blackjack to start one.');
-        }
-        break;
-    
-      default:
-        break;
-    }
-  }
-
-  startGame(user, parsedCommands) {
-    switch (parsedCommands.start.toLowerCase()) {
-      case 'bj':
-        if (!this.games.bj) {
-          let cards = new Deck(constants.BLACKJACK);
-          this.games.bj = new BlackJack(cards, user, parsedCommands, this.reply, this.playerCoins);
-        }
-        else {
-          this.emitWarning(user, 'A game of blackjack is already in progress. Type /cards --join bj to join.');
-        }
-        break;
-    
-      default:
-        break;
-    }
   }
 
   extractCommands(commands) {
-    let parsedCommands = {
-      
-    };
+    let parsedCommands = {};
     let isValid = true;
     let reason = "";
+    
     for (let i = 0; i < commands.length; i++) {
+
       if (commands[i].startsWith('--')) {
         parsedCommands[commands[i].slice(2)] = commands[i + 1];
         i++;
@@ -162,11 +49,116 @@ module.exports = class Cards {
     }
   }
 
+  getHelp(user, parsedCommands) {
+
+    if (parsedCommands.help === 'all') {
+      this.availableCommands.forEach(command => {
+        this.sendMessage(`${command}`, 'white', false, user);
+      });
+    }
+    else if (parsedCommands.help === 'bj') {
+      [
+        `\'/cards -bj -deal\' - Start a game with existing players`,
+        `\'/cards -bj -hit\' - Hit to draw another card`,
+        `\'/cards -bj -stand\' - Stand to keep your current card total`,
+        `\'/cards -bj -double\' - Double your bet and draw exactly once more (feature still in development)`,
+      ].forEach(command => {
+        this.sendMessage(`${command}`, 'white', false, user);
+      });
+    }
+    else {
+      this.sendMessage('Valid options for help: \'/cards --help all\' and \'/cards --help bj\' (Blackjack)', 'red', false, user);
+    }
+  }
+
   isValidCommands(parsedCommands) {
     return true;
   }
 
+  joinGame(user, parsedCommands) {
+
+    switch (parsedCommands.join.toLowerCase()) {
+      case 'bj':
+        if (this.games.bj) {
+          this.games.bj.addPlayer(user);
+        }
+        else {
+          this.sendMessage('There is no open game of blackjack. Type /cards --start blackjack to start one.', 'red', false, user);
+        }
+        break;
+    
+      default:
+        break;
+    }
+  }
+
   parseReason(command) {
     return `\'${command}\' is not a valid command. Please input \'/cards --help all\' for more information.`;
+  }
+
+  receiveCommand(user, command) {
+
+    const parsedCommands = this.extractCommands(command);
+
+    if (parsedCommands.isValid) {
+      if (parsedCommands.start) {
+        this.startGame(user, parsedCommands);
+      }
+      else if (parsedCommands.help) {
+        this.getHelp(user, parsedCommands);
+      }
+      else if (parsedCommands.join) {
+        this.joinGame(user, parsedCommands);
+      }
+      else if (parsedCommands.bj && this.games.bj) {
+        this.games.bj.receiveCommand(user, parsedCommands);
+      }
+      else {
+        this.sendMessage('You must issue one of the following commands: \'/cards --start [game] -[#players]\',\'/cards --join [game]\', or \'--help [game]/all\'', 'red', false, user);
+      }
+    }
+    else {
+      this.sendMessage(parsedCommands.reason, 'red', false, user);
+    }
+  }
+
+  sendMessage(message, color, broadcast = true, user = false) {
+
+    if (broadcast) {
+      this.reply({
+        broadcast: true,
+        message: message,
+        styling: {
+          color: color
+        }
+      });
+    }
+    else {
+      this.reply({
+        user: user,
+        message: message,
+        styling: {
+          color: color
+        }
+      });
+    }
+  }
+
+  startGame(user, parsedCommands) {
+
+    switch (parsedCommands.start.toLowerCase()) {
+      case 'bj':
+        if (!this.games.bj) {
+          let cards = new Deck(constants.BLACKJACK);
+          this.games.bj = new BlackJack(cards, user, parsedCommands, this.reply, this.playerCoins);
+        }
+        else {
+          this.sendMessage('A game of blackjack is already in progress. Type /cards --join bj to join.', 'red', false, user);
+        }
+        break;
+    
+      default:
+        break;
+    }
   }
 };
