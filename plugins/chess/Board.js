43c8903302997d8a0cd146a6094775c7dffdae93
Robert Taussig <robert.taussig@gmail.com>
2017-06-11 19:44:16 -0400

    Begin legal move check flow

--- plugins/chess/Board.js
+++ plugins/chess/Board.js
@@ -1,9 +1,176 @@
 module.exports = class Board {
   constructor() {
     this.board = 'rnbqkbnrpppppppp--------------------------------PPPPPPPPRNBQKBNR';
+    this.currentTurn = 'w';
+    this.legalMoves = this.findLegalMoves(this.board, this.currentTurn);
+  }
+
+  findLegalMoves(board, currentTurn) {
+    let legalMoves = [];
+    let move;
+    for (let i = 0; i < board.length; i++) {
+      switch (board[i]) {
+
+        case 'r':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getRookMoves(i, board));
+          }
+          break;
+
+        case 'n':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getKnightMoves(i, board));
+          }
+          break;
+
+        case 'b':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getRookMoves(i, board));
+          }
+          break;
+
+        case 'q':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getQueenMoves(i, board));
+          }
+          break;
+
+        case 'k':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getKingMoves(i, board));
+          }
+          break;
+
+        case 'p':
+          if (currentTurn === 'b') {
+            legalMoves = legalMoves.concat(this.getPawnMoves(i, board));
+          }
+          break;
+
+        case 'R':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getRookMoves(i, board));
+          }
+          break;
+        
+        case 'N':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getKnightMoves(i, board));
+          }
+          break;
+
+        case 'B':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getBishopMoves(i, board));
+          }
+          break;
+        
+        case 'Q':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getQueenMoves(i, board));
+          }
+          break;
+
+        case 'K':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getKingMoves(i, board));
+          }
+          break;
+
+        case 'P':
+          if (currentTurn === 'w') {
+            legalMoves = legalMoves.concat(this.getPawnMoves(i, board));
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    return legalMoves.filter( el => {
+      return this.isCheck(currentTurn, this.testMove(el, board));
+    });
+  }
+
+  /*
+  0  1  2  3  4  5  6  7
+  8  9  10 11 12 13 14 15
+  16 17 18 19 20 21 22 23
+  24 25 26 27 28 29 30 31
+  32 33 34 35 36 37 38 39
+  40 41 42 43 44 45 46 47
+  48 49 50 51 52 53 54 55
+  56 57 58 59 60 61 62 63
+  */
+
+  getBishopMoves(position, board) {
+    let increments = [9, 7, -9, -7];
+    let color = this.getColor(position, board);
+    let legalMoves = [];
+    for (let i = 0; i < increments.length; i++) {
+      let pointer = position;
+      while (pointer < 63 && pointer > 0 && (pointer + increments[i]) % 8 !== 0) {
+        pointer += increments[i];
+        if (board[pointer] === '-') {
+          legalMoves.push(`${position}-${board[pointer]}`);
+        } else if (this.getColor(board[pointer]) !== color) {
+          legalMoves.push(`${position}-${board[pointer]}`);
+          break;
+        } else {
+          break;
+        }
+      }
+    }
+    return legalMoves;
   }
 
   getBoard() {
     return this.board;
+  }
+
+  getColor(position, board) {
+    return board[position].toUpperCase() === board[position] ? 'w' : 'b';
+  }
+
+  getKnightMoves(position, board) {
+
+  }
+
+  getKingMoves(position, board) {
+
+  }
+
+  getPawnMoves(position, board) {
+
+  }
+
+  getQueenMoves(position, board) {
+    
+  }
+
+  getRookMoves(position, board) {
+
+  }
+  
+  isCheck(currentPlayer, board) {
+    // Check for attackers
+    return false;
+  }
+
+  isLegalMove(from, to) {
+    return Boolean(this.legalMoves.includes(`${from}-${to}`));
+  }
+
+  makeMove(from, to) {
+
+  }
+
+  testMove(move, board) {
+    let from = move.split('-')[0];
+    let to = move.split('-')[1];
+    let placePiece = board.substr(0, to) + board[from] + board.substr(to + 1);
+    let removePiece = placePiece.substr(0, from) + '-' + placePiece.substr(from + 1);
+    return removePiece;
   }
 };
--- plugins/chess/Game.js
+++ plugins/chess/Game.js
@@ -16,6 +16,16 @@ const pieceMap = {
   'P': '<span class="w" style="color: lightseagreen">♟</span>'
 };
 
+const posMap = {
+  'a8': 0,  'b8': 1,  'c8': 2,  'd8': 3,  'e8': 4,  'f8': 5,  'g8': 6,  'h8': 7,  'a7': 8,  'b7': 9, 
+  'c7': 10, 'd7': 11, 'e7': 12, 'f7': 13, 'g7': 14, 'h7': 15, 'a6': 16, 'b6': 17, 'c6': 18, 'd6': 19, 
+  'e6': 20, 'f6': 21, 'g6': 22, 'h6': 23, 'a5': 24, 'b5': 25, 'c5': 26, 'd5': 27, 'e5': 28, 'f5': 29, 
+  'g5': 30, 'h5': 31, 'a4': 32, 'b4': 33, 'c4': 34, 'd4': 35, 'e4': 36, 'f4': 37, 'g4': 38, 'h4': 39, 
+  'a3': 40, 'b3': 41, 'c3': 42, 'd3': 43, 'e3': 44, 'f3': 45, 'g3': 46, 'h3': 47, 'a2': 48, 'b2': 49, 
+  'c2': 50, 'd2': 51, 'e2': 52, 'f2': 53, 'g2': 54, 'h2': 55, 'a1': 56, 'b1': 57, 'c1': 58, 'd1': 59, 
+  'e1': 60, 'f1': 61, 'g1': 62, 'h1': 63
+};
+
 module.exports = class Game {
   constructor(user) {
     this.users = {
@@ -24,7 +34,7 @@ module.exports = class Game {
         color: 'w'
       }
     };
-    this.currentTurn = user;
+    this.currentTurn = user.id;
     this.board = new Board();
   }
 
@@ -34,7 +44,7 @@ module.exports = class Game {
     return `
       <script>
         var playerColor = '${color}';
-        var currentPlayer = '${this.currentTurn.id}' === '${user.id}';
+        var currentPlayer = '${this.currentTurn}' === '${user.id}';
 
         var firstMove = function(pColor, cPlayer) {
           $('.chess-board div').each(function(idx) {
@@ -220,11 +230,41 @@ module.exports = class Game {
     `;
   }
 
+  hasOpenSpot() {
+    return (Object.keys(this.users).length < 2);
+  }
+
   joinGame(user) {
     this.users[user.id] = {
       user: user,
       color: 'w'
     };
+  }
+
+  makeMove(user, from, to, sendMessage, render) {
+    if (this.currentTurn === user.id) {
+      if (this.board.isLegalMove(posMap[from], posMap[to])) {
+        this.board.makeMove(posMap[from], posMap[to]);
+        for (let player in this.users) {
+          if (this.users.hasOwnProperty(player)) {
+            this.renderBoard(this.users[player].user,render);
+          }
+        }
+      } else {
+        sendMessage('That is not a valid move.', 'red', false, user);
+        this.renderBoardState(user,render);
+      }
+    } else {
+      sendMessage('It is not your turn to move.', 'red', false, user);
+    }
+  }
+
+  renderBoardState(user, render) {
+    let html = this.generateHtml(user);
+    render({
+      user: user,
+      html: html
+    });
   }
 
   watchGame(user) {
